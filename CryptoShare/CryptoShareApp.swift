163a30d78e682528c32ca59f27270320eb1521d5
fe <fatiherdgn25@gmail.com>
2023-06-23 09:27:12 +0300

    Initial Commit

---
+++ CryptoShare/CryptoShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoShareApp.swift
+//  CryptoShare
+//
+//  Created by fe on 23.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
